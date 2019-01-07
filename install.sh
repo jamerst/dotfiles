4e5f2dee7a693fdb4dbdb8320ffeeee21881ceda
James Tattersall <10601770+jamerst@users.noreply.github.com>
2019-01-07 16:29:07 +0000

    Fix conky and oh-my-zsh (archive to preserve git repos)

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ wd="$(pwd)"
 ln -sfv $wd/ckb-next/ckb-next.conf $HOME/.config/ckb-next/ckb-next.conf
 
 # CONKY
+tar -xf conky/conky-modern.tar.gz -C conky
 ln -sfv $wd/conky/conky-modern/conky-modern.conky $HOME/.conkyrc
 
 # CSSCOMB
@@ -22,6 +23,7 @@ ln -sfv $wd/nemo/local $HOME/.local/share/nemo
 ln -sfv $wd/nemo/Templates $HOME/Templates
 
 # ZSH
+tar -xf zsh/oh-my-zsh.tar.gz -C zsh
 ln -sfv $wd/zsh/oh-my-zsh $HOME/.oh-my-zsh
 ln -sfv $wd/zsh/oh-my-zsh/zshrc $HOME/.zshrc
 ln -sfv $wd/zsh/z $HOME/.z
---
+++ package.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+tar -czf conky/conky-modern.tar.gz -C conky conky-modern
+tar -czf zsh/oh-my-zsh.tar.gz -C zsh oh-my-zsh
