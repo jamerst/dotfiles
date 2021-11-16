155f5412a591b3274ad9578d349fe886e33295b3
James Tattersall <10601770+jamerst@users.noreply.github.com>
2021-11-16 23:45:46 +0000

    Add fzf-tab

--- zsh/zsh_plugins.sh
+++ zsh/zsh_plugins.sh
@@ -12,3 +12,5 @@ source /home/james/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zd
 fpath+=( /home/james/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
 source /home/james/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /home/james/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/james/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
+fpath+=( /home/james/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
