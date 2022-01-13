88737bea2015422898db26d3fa6fd6a30a49a053
James Tattersall <10601770+jamerst@users.noreply.github.com>
2022-01-13 21:41:30 +0000

    Switch hook script to brightnessd

--- redshift/hooks/brightness.sh
+++ redshift/hooks/brightness.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Set brightness for DDC/CI enabled monitors
-# Originally by rnhmjoj, modified slightly by jamerst
-# https://github.com/jonls/redshift/issues/436#issuecomment-366402503
+# Set brightness for DDC/CI enabled monitors using brightnessd
 
 # Place in ~/.config/redshift/hooks
 # See HOOKS section under man page of redshift for more information about redshift hooks
 
 # brightness values for each period (transition is brightness inbetween, not transition time)
-night=10
+night=5
 transition=10
 daytime=50
 
@@ -19,31 +17,16 @@ deref() {
 }
 
 fade_to() {
-  current=$(ddcutil get 10 | cut -d, -f1 | cut -d= -f2)
-  [ "$current" -gt "$1" ] && step=-1 || step=1
-  # set dim direction based on current brightness being greater or less than target
-  for i in $(seq "$current" "$step" "$1"); do
-    if [ -f /tmp/redshift-brightness-halt ]; then
-      rm /tmp/redshift-brightness-halt
-      break
-    fi
-
-    # step brightness down 1 level at a time
-    ddcutil set 10 $i -d 1
-    ddcutil set 10 $i -d 2
-    sleep $2
-  done
+  dbus-send --print-reply --dest=net.jtattersall.brightness /net/jtattersall/brightness net.jtattersall.brightness.Stop
+  dbus-send --print-reply --dest=net.jtattersall.brightness /net/jtattersall/brightness net.jtattersall.brightness.Fade int32:$1 int32:$2
 }
 
-
-# if hook is for period changing and not already running
-if [ "$1" = "period-changed" ] && [ ! -f /tmp/redshift-brightness-lock ]; then
-  touch /tmp/redshift-brightness-lock
+# if hook is for period changing
+if [ "$1" = "period-changed" ]; then
   # if changing at startup, i.e. not fading between states
   if [ "$2" = "none" ]; then
-    fade_to "$(deref "$3")" 0.1
+    fade_to "$(deref "$3")" 15
   else
-    fade_to "$(deref "$3")" 60
+    fade_to "$(deref "$3")" 3600
   fi
-  rm /tmp/redshift-brightness-lock
 fi
